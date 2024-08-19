1450f22b01fbba1ae4cefbba90e7321084e3530c
Migue <miguelulmi@hotmail.com>
2024-08-19 15:44:49 -0300

    Update app.js

--- app.js
+++ app.js
@@ -21,13 +21,23 @@ function secretNumber(){
 function guess(){
     let usuario = selecValue("number");
     let number = secretNumber();
-    let i = 0;
+    let i = 1;
     
-    while (i < 3 ){
+    while (i < 4 ){
         if (usuario === number){
-
+            printScreen("title", "GANASTE!!!");
+            printScreen("text-title", `Lo hiciste en ${i} intentos`);
+            break;
         }else if (number < usuario){
-            
+            printScreen("text-title", `El número secreto es menor`);
+        }else if (number > usuario){
+            printScreen("text-title", `El número secreto es mayor`);
+        }else if (i === 3){
+            printScreen("title", "FALLASTE! No hay mas intentos!");
+            printScreen("text-title", `El número secreto era ${number}`);
         }
+
+        printScreen("title", `Ingrese el intento ${i}:`);
+        i++;
     }
 }
